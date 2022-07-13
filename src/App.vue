66ad390fcee18aaa6ad216d762e062ccbd9fc975
caipaofan <954698621@qq.com>
2022-07-13 12:07:08 +0800

    完成了项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
-    <h1>黑马头疼</h1>
+    <h1><span class="toutiao toutiao-dianzan"></span> 黑马头疼</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="login"></div>
   </div>
 </template>
 
 <style lang="less">
-
+.login {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
 </style>
