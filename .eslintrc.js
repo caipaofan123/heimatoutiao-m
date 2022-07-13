2c4d414a93dbd6e5af1cf487c6b001860e669ed3
caipaofan <954698621@qq.com>
2022-07-13 09:46:48 +0800

    完成了清理

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,13 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-multiple-empty-lines': 0,
+    'no-trailing-spaces': 0
   }
 }
